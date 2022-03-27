dd2060dae8690b0c89c623e870a8fb8b810d8652
parth <parthrambhhia25@gmail.com>
2022-03-27 16:23:47 +0530

    edit database files

--- py/xyz.py
+++ py/xyz.py
@@ -4,6 +4,7 @@ from register1 import Ui_Register1
 from register2 import Ui_Register2
 from homepage import Ui_homepage
 from l_homepage import Ui_l_homepage
+from db import DatabaseConnection
 
 class Login(QtWidgets.QMainWindow,Ui_Login):
     def __init__(self) -> None:
@@ -14,6 +15,27 @@ class Register1(QtWidgets.QMainWindow,Ui_Register1):
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
+    def reg(self):
+        n = self.tname.text()
+        em = self.temail.text()
+        ph = self.tphone.text()
+        dob = self.dateEdit.text()
+        un = self.tusername.text()
+        pw = self.tpass.text()
+        re = self.trepass.text()
+
+        db = DatabaseConnection()
+        cursor = db.cursor()
+        cursor.execute("select * from userlist where username= '"+un+"' and password = '"+ pw +"' ")
+        result = cursor.fetchone()
+
+        if result:
+            QMessageBox.information(self, "Login form", "User already exists!")
+        else:
+            cursor.execute("insert into register values('"+ n +"','"+ em +"','"+ ph +"','"+ dob +"','"+ un +"','"+ pw +"','"+ re +"')")
+
+            db.commit()
+            QMessageBox.information(self, "Login form", "Registered Successfully")
 
 class Register2(QtWidgets.QMainWindow,Ui_Register2):
     def __init__(self) -> None:
