41245beb88f81e885907ca78f2c50f05a91f391b
Parth25402 <parthrambhia25@gmail.com>
2022-03-28 20:09:02 +0530

    Merge branch 'main' of https://github.com/neel-mistry/PoCoH-MiniProj

--- py/xyz.py
+++ py/xyz.py
@@ -6,6 +6,7 @@ from homepage import Ui_homepage
 from l_homepage import Ui_l_homepage
 from register_new import Ui_register_new
 from precautions import Ui_Precautions
+from db import DatabaseConnection
 
 class Login(QtWidgets.QMainWindow,Ui_Login):
     def __init__(self) -> None:
@@ -16,27 +17,6 @@ class Register1(QtWidgets.QMainWindow,Ui_Register1):
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
-    def reg(self):
-        n = self.tname.text()
-        em = self.temail.text()
-        ph = self.tphone.text()
-        dob = self.dateEdit.text()
-        un = self.tusername.text()
-        pw = self.tpass.text()
-        re = self.trepass.text()
-
-        db = DatabaseConnection()
-        cursor = db.cursor()
-        cursor.execute("select * from userlist where username= '"+un+"' and password = '"+ pw +"' ")
-        result = cursor.fetchone()
-
-        if result:
-            QMessageBox.information(self, "Login form", "User already exists!")
-        else:
-            cursor.execute("insert into register values('"+ n +"','"+ em +"','"+ ph +"','"+ dob +"','"+ un +"','"+ pw +"','"+ re +"')")
-
-            db.commit()
-            QMessageBox.information(self, "Login form", "Registered Successfully")
 
 class Register2(QtWidgets.QMainWindow,Ui_Register2):
     def __init__(self) -> None:
@@ -57,6 +37,29 @@ class N_Register(QtWidgets.QMainWindow,Ui_register_new):
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
+        self.bsubmit.clicked.connect(self.reg)
+
+    def reg(self):
+        n = self.tname.text()
+        em = self.temail.text()
+        ph = self.tphone.text()
+        dob = self.dateEdit.text()
+        un = self.tusername.text()
+        pw = self.tpass.text()
+        re = self.trepass.text()
+
+        db = DatabaseConnection()
+        cursor = db.cursor()
+        cursor.execute("select * from register where username= '"+un+"' and Pwd = '"+ pw +"' ")
+        result = cursor.fetchone()
+
+        if result:
+            QtWidgets.QMessageBox.information(self, "Login form", "User already exists!")
+        else:
+            cursor.execute("insert into register values('"+ n +"','"+ em +"','"+ ph +"','"+ dob +"','"+ un +"','"+ pw +"','"+ re +"')")
+
+            db.commit()
+            QtWidgets.QMessageBox.information(self, "Login form", "Registered Successfully")
 
 class Precautions(QtWidgets.QMainWindow,Ui_Precautions):
     def __init__(self) -> None:
